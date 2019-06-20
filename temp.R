7cf8ae2cc7c2ae31985fad804b6614bd11e4984b
Leo <jlm0808@126.com>
2019-06-20 12:52:18 +0800

    edit app.R

---
+++ temp.R
@@ -0,0 +1,20 @@
+loc <- "http://archive.ics.uci.edu/ml/machine-learning-databases/"
+ds <- "breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+url <- paste(loc, ds, sep="")
+breast <- read.table(url, sep=",", header=FALSE, na.strings="?")
+names(breast) <- c("ID", "clumpThickness", "sizeUniformity",
+                   "shapeUniformity", "maginalAdhesion",
+                   "singleEpithelialCellSize", "bareNuclei",
+                   "blandChromatin", "normalNucleoli", "mitosis", "class")
+df <- breast[-1]
+df$class <- factor(df$class, levels=c(2,4),
+                   labels=c("benign", "malignant"))
+set.seed(1234)
+train <- sample(nrow(df), 0.7*nrow(df))
+df.train <- df[train,]
+df.validate <- df[-train,]
+table(df.train$class)
+table(df.validate$class)
+
+fit.logit <- glm(class~., data=df.train, family=binomial())
+summary(fit.logit)
