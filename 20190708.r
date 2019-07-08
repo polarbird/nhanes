12336cfa6a404bfe95e5d65e14f86bae3b9710ed
Leo <jlm0808@126.com>
2019-07-08 20:02:30 +0800

    add data source

--- 20190708.r
+++ 20190708.r
@@ -18,7 +18,7 @@ full_str <- c("DEMO_!demo_obj<-temp_var!tp_var  <- data.frame(seqn=xpt_var$seqn,
               "BPX_!bpx_obj<-temp_var!tp_var <- data.frame(seqn=xpt_var$seqn,bpsy1=xpt_var$bpxsy1,bpdi1=xpt_var$bpxdi1,bpsy2=xpt_var$bpxsy2,bpdi2=xpt_var$bpxdi2,bpsy3=xpt_var$bpxsy3,bpdi3=xpt_var$bpxdi3)",
               "BMX_!bmx_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,bmi=xpt_var$bmxbmi)",
               "ALB_!alb_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,acr=xpt_var$urdact)",
-              "PBCD_!pbcd_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,pb=xpt_var$lbxbpb,cd=xpt_var$lbxbcd,hg=xpt_var$lbxthg,se=xpt_var$lbxbse,mn=xpt_var$lbxbmn)",
+              "PBCD_!pbcd_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,wtsh2yr=xpt_var$wtsh2yr,pb=xpt_var$lbxbpb,cd=xpt_var$lbxbcd,hg=xpt_var$lbxthg,se=xpt_var$lbxbse,mn=xpt_var$lbxbmn)",
               "GHB_!ghb_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,hba1c=xpt_var$lbxgh)",
               "BIOPRO_!biopro_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,alb=xpt_var$lbdsalsi,alt=xpt_var$lbxsatsi,ast=xpt_var$lbxsassi,bun=xpt_var$lbxsbu,scr=xpt_var$lbxscr,ua=xpt_var$lbxsua,chl=xpt_var$lbxsch,tri=xpt_var$lbxstr)",
               "ALQ_!alq_obj<-temp_var!tp_var<- data.frame(seqn=xpt_var$seqn,alq101=xpt_var$alq101,alq110=xpt_var$alq110,alq120q=xpt_var$alq120q)",
@@ -32,6 +32,7 @@ full_str <- c("DEMO_!demo_obj<-temp_var!tp_var  <- data.frame(seqn=xpt_var$seqn,
 
 for(single_str in full_str){
   data_str <- strsplit(single_str,"!")
+  print(data_str)
   file_str <- data_str[[1]][1]
   obj_str <- data_str[[1]][2]
   action_str <- data_str[[1]][3]
@@ -41,6 +42,7 @@ for(single_str in full_str){
   temp_var <- 0L
   for( f in file_var){
     xpt_var <-sasxport.get(f)
+    summary(xpt_var)
     eval(parse(text = action_str))
     if (is.integer(temp_var)) {
       temp_var <- tp_var
@@ -69,3 +71,5 @@ write.table(final_results ,"../mydata.csv",sep=",",row.names = FALSE)
 
 #library(xlsx) 
 #write.xlsx(final_results, "d:/mydata.xlsx")
+
+print(full_str)
---
+++ origin/test.r
@@ -0,0 +1,5 @@
+library(Hmisc)
+
+setwd("D:/work/nhanes/origin")
+
+pbcd_source <- sasxport.get("PBCD_G.XPT")
